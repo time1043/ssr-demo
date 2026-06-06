09108b54182cd700a8fdac41bc8ac56a57d9a02b
oswin902m <1665434994@qq.com>
2026-06-06 23:36:35 +0800

    feat: add CSR advice demo via React

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+
+type AdviceResponse = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+async function getAdviceApi() {
+  // https://api.adviceslip.com/
+  const response = await fetch("https://api.adviceslip.com/advice");
+  return (await response.json()) as AdviceResponse;
+}
+
 export default function App() {
-  return <div>App</div>;
+  const [advice, setAdvice] = useState("Advice ...");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function getAdvice() {
+    setIsLoading(true);
+
+    const data = await getAdviceApi();
+
+    setAdvice(data.slip.advice);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <main>
+      <h1>Advice App - CSR Version</h1>
+      <p>{isLoading ? "Loading..." : advice}</p>
+      <button onClick={getAdvice} disabled={isLoading}>
+        Get Advice
+      </button>
+    </main>
+  );
 }
