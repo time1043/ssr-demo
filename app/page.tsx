9f25c4e3d8060d81f351af6d79a597894cb4c883
oswin <1665434994@qq.com>
2026-06-09 20:23:15 +0800

    feat: React Server Component without Client Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,13 @@
-"use client";
-
 import { getAdviceApi } from "@/lib/active";
-import { useEffect, useState } from "react";
 
-export default function Page() {
-  const [advice, setAdvice] = useState("Advice ...");
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function getAdvice() {
-    setIsLoading(true);
-
-    const data = await getAdviceApi();
-
-    setAdvice(data.slip.advice);
-    setIsLoading(false);
-  }
-
-  useEffect(() => {
-    getAdvice();
-  }, []);
+export default async function Page() {
+  const data = await getAdviceApi();
 
   return (
     <main>
-      <h1>Advice App - NextJS Client Component Version</h1>
-      <p>{isLoading ? "Loading..." : advice}</p>
-      <button onClick={getAdvice} disabled={isLoading}>
-        Get Advice
-      </button>
+      <h1>Advice App - NextJS RSC Version</h1>
+      <p>{data.slip.advice}</p>
+      <button>Get Advice</button>
     </main>
   );
 }
