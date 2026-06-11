eb28b3e720e455e3865ead7889dbe9bfc9c4f062
oswin <1665434994@qq.com>
2026-06-11 17:57:46 +0800

    feat: simulate the request interface and wait for 5s

--- lib/active.ts
+++ lib/active.ts
@@ -6,6 +6,7 @@ export type AdviceResponse = {
 };
 
 export async function getAdviceApi() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   // https://api.adviceslip.com/
   const res = await fetch("https://api.adviceslip.com/advice");
   return (await res.json()) as AdviceResponse;
